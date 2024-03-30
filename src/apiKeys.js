06d1ab7cbaaf3a0d322abe9cd8aad74c594ca62c
ADNAN CHAUDHARY <chaudharyadnan00@gmail.com>
2024-03-30 14:48:14 +0530

    [ADD] weatherapp: built a weather app
    -First of all it ask the user to allow current location
    -Tells us about the user current location temprature,humidity
    ,wind speed, visibility
    -User can search for other locations also

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "6e11073c89e4974ccc16b25e1bbe91d2",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
